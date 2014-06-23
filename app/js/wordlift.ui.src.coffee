dae65e4e639efbc7e1bff092a2a95d38a81698d4
pieroit <savastanos@libero.it>
2014-06-23 10:51:38 +0200

    add beautifyLabel to chord, transform chord in a jquery plugin

--- app/js/wordlift.ui.src.coffee
+++ app/js/wordlift.ui.src.coffee
@@ -1,200 +1,223 @@
 $ = jQuery
 
-getChordData = (params) ->
-  $.post params.ajax_url, { action: params.action, post_id: params.postId, depth: params.depth }, (data) ->
-    buildChord data, params
+# Add a chord plugin object to jQuery
+$.fn.extend
+  chord: (options) ->
+    
+    # Default settings
+    settings = {}
 
-buildChord = (data, params) ->
-  return if data.entities.length < 2
+    # Merge default settings with options.
+    settings = $.extend settings, options
 
-#  niceData = 'entities' in dataMock && 'relations' in dataMock
-  #  niceData = niceData && (dataMock.entities.length) >= 2 && (dataMock.relations >= 1)
-  #  if ( niceData )
-  #    d3.select( '#' + wl_chord_params.widget_id )
-  #     .style('height', '50px')
-  #     .html(' --- WordLift shortcode: No entities found. --- ')
-  #  return;
-  #}
-
-  translate = (x, y, size) -> 'translate(' + x * size + ',' + y * size + ')'
-
-  rotate = (x) -> "rotate(#{x})"
-
-  rad2deg = (a) ->  ( a / (2*Math.PI)) * 360
-
-  sign = (n) -> if n >= 0.0 then 1 else -1
-
-  beautifyLabel = (txt) ->
-    return txt.substring(0, 12) + '...' if txt.length > 12
-    txt
-
-  colorLuminance = (hex, lum) ->
-    # Validate hex string.
-    hex = String(hex).replace(/[^0-9a-f]/gi, '')
-
-    if (hex.length < 6)
-      hex = hex[0]+hex[0]+hex[1]+hex[1]+hex[2]+hex[2]
-
-    lum = lum or 0
-
-    # Convert to decimal and change luminosity.
-    rgb = "#"
-    c = undefined
-    i = undefined
-
-    for i in [0..3]
-      c = parseInt(hex.substr(i*2,2), 16)
-      c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16)
-      rgb += ("00"+c).substr(c.length)
-
-    rgb
-
-  getEntityIndex = (uri) ->
-    return i for i in [0..data.entities.length] when data.entities[i].uri is uri
-    -1
-
-# Build adiacency matrix.
-  matrix = []
-  matrix.push (0 for e in data.entities) for entity in data.entities
-
-  for relation in data.relations
-    x = getEntityIndex relation.s
-    y = getEntityIndex relation.o
-    matrix[x][y] = 1
-    matrix[y][x] = 1
+    # Initialization method
+    init = ->
+      getChordData()
   
-  viz = d3.select('#' + params.widget_id ).append('svg')
-  viz.attr('width', '100%').attr('height', '100%')
-
-  # Getting dimensions in pixels.
-  width = parseInt(viz.style('width'))
-  height = parseInt(viz.style('height'))
-  size =  if height < width then height else width
-  innerRadius = size*0.2
-  outerRadius = size*0.25
-  arc = d3.svg.arc().innerRadius(innerRadius).outerRadius(outerRadius)
-
-  chord = d3.layout.chord()
-    .padding(0.3)
-    .matrix(matrix)
-
-  # Draw relations.
-  viz.selectAll('chords')
-    .data(chord.chords)
-    .enter()
-    .append('path')
-    .attr('class', 'relation')
-    .attr('d', d3.svg.chord().radius(innerRadius))
-    .attr('transform', translate(0.5, 0.5, size) )
-    .style('opacity', 0.2)
-    .on('mouseover', ->  d3.select(this).style('opacity', 0.8) )
-    .on('mouseout', ->  d3.select(this).style('opacity', 0.2) )
-
-  # Draw entities.
-  viz.selectAll('arcs')
-    .data(chord.groups)
-    .enter()
-    .append('path')
-    .attr('class', (d) ->
-      return "entity #{data.entities[d.index].css_class}"
-    )
-    .attr('d', arc)
-    .attr('transform', translate(0.5, 0.5, size))
-    .style('fill', (d) ->
-      baseColor = params.mainColor;
-      type = data.entities[d.index].type
-      return baseColor if(type == 'post')
-      return colorLuminance( baseColor, -0.5) if type is 'entity'
-      colorLuminance( baseColor, 0.5 )
-    )
-
-  # Draw entity labels.
-  viz.selectAll('arcs_labels')
-    .data(chord.groups)
-    .enter()
-    .append('text')
-    .attr('class', 'label')
-#    .html( (d) ->
-#      lab = data.entities[d.index].label
-#      beautifyLabel(lab)
-#    )
-    .attr('font-size', ->
-      fontSize = parseInt( size/35 )
-      fontSize = 8 if(fontSize < 8)
-      fontSize + 'px'
-    )
-    .each( (d) ->
-      n = data.entities[d.index].label.split(/\s/)
-
-      # get the current element
-      text = d3.select(this)
-        .attr("dy", n.length / 3 - (n.length-1) * 0.9 + 'em')
-        .html(n[0])
-
-      # now loop
-      for i in [1..n.length]
-        text.append("tspan")
-        .attr('x', 0)
-        .attr('dy', '1em')
-        .html(n[i])
-
-      text.attr('transform', (d) ->
-        alpha = d.startAngle - Math.PI/2 + Math.abs((d.endAngle - d.startAngle)/2)
-        labelWidth = 3
-        labelAngle = undefined
-        if(alpha > Math.PI/2)
-          labelAngle = alpha - Math.PI
-          labelWidth += d3.select(this)[0][0].clientWidth
-        else
-          labelAngle = alpha
-
-        labelAngle = rad2deg( labelAngle )
-
-        r = (outerRadius + labelWidth)/size
-        x = 0.5 + ( r * Math.cos(alpha) )
-        y = 0.5 + ( r * Math.sin(alpha) )
-
-        translate(x, y, size) + rotate( labelAngle )
+    getChordData = ->
+      $.post settings.ajax_url, { action: settings.action, post_id: settings.postId, depth: settings.depth }, (data) ->
+        buildChord data
+    
+    buildChord = (data) ->
+      return if data.entities.length < 2
+    
+      translate = (x, y, size) -> 'translate(' + x * size + ',' + y * size + ')'
+    
+      rotate = (x) -> "rotate(#{x})"
+    
+      rad2deg = (a) ->  ( a / (2*Math.PI)) * 360
+    
+      sign = (n) -> if n >= 0.0 then 1 else -1
+      
+      beautifyLabel = (words) ->
+        # when labels are way too long, show only the first part of them
+        # (the whole label will be displayed in the tooltip at mouseover) 
+        maxLabelLenght = 30
+        if words.length > maxLabelLenght
+          words = words.substring(0, maxLabelLenght) + '...'
+        
+        # split in words
+        words = words.split(/\s/)
+        
+        # group shortest words
+        n = []
+        maxWordLength = 5;
+        for w in [0..words.length-1]
+          if words[w].length > maxWordLength or w is words.length-1
+            n.push words[w]
+          else
+            words[w+1] = words[w] + ' ' + words[w+1]
+        
+        return n
+    
+      colorLuminance = (hex, lum) ->
+        # Validate hex string.
+        hex = String(hex).replace(/[^0-9a-f]/gi, '')
+    
+        if (hex.length < 6)
+          hex = hex[0]+hex[0]+hex[1]+hex[1]+hex[2]+hex[2]
+    
+        lum = lum or 0
+    
+        # Convert to decimal and change luminosity.
+        rgb = "#"
+        c = undefined
+        i = undefined
+    
+        for i in [0..3]
+          c = parseInt(hex.substr(i*2,2), 16)
+          c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16)
+          rgb += ("00"+c).substr(c.length)
+    
+        rgb
+    
+      getEntityIndex = (uri) ->
+        return i for i in [0..data.entities.length] when data.entities[i].uri is uri
+        -1
+    
+      # Build adiacency matrix.
+      matrix = []
+      matrix.push (0 for e in data.entities) for entity in data.entities
+    
+      for relation in data.relations
+        x = getEntityIndex relation.s
+        y = getEntityIndex relation.o
+        matrix[x][y] = 1
+        matrix[y][x] = 1
+      
+      viz = d3.select('#' + settings.widget_id ).append('svg')
+      viz.attr('width', '100%').attr('height', '100%')
+    
+      # Getting dimensions in pixels.
+      width = parseInt(viz.style('width'))
+      height = parseInt(viz.style('height'))
+      size =  if height < width then height else width
+      innerRadius = size*0.2
+      outerRadius = size*0.25
+      arc = d3.svg.arc().innerRadius(innerRadius).outerRadius(outerRadius)
+    
+      chord = d3.layout.chord()
+        .padding(0.3)
+        .matrix(matrix)
+    
+      # Draw relations.
+      viz.selectAll('chords')
+        .data(chord.chords)
+        .enter()
+        .append('path')
+        .attr('class', 'relation')
+        .attr('d', d3.svg.chord().radius(innerRadius))
+        .attr('transform', translate(0.5, 0.5, size) )
+        .style('opacity', 0.2)
+        .on('mouseover', ->  d3.select(this).style('opacity', 0.8) )
+        .on('mouseout', ->  d3.select(this).style('opacity', 0.2) )
+    
+      # Draw entities.
+      viz.selectAll('arcs')
+        .data(chord.groups)
+        .enter()
+        .append('path')
+        .attr('class', (d) ->
+          return "entity #{data.entities[d.index].css_class}"
+        )
+        .attr('d', arc)
+        .attr('transform', translate(0.5, 0.5, size))
+        .style('fill', (d) ->
+          baseColor = settings.mainColor;
+          type = data.entities[d.index].type
+          return baseColor if(type == 'post')
+          return colorLuminance( baseColor, -0.5) if type is 'entity'
+          colorLuminance( baseColor, 0.5 )
+        )
+    
+      # Draw entity labels.
+      viz.selectAll('arcs_labels')
+        .data(chord.groups)
+        .enter()
+        .append('text')
+        .attr('class', 'label')
+        .attr('font-size', ->
+          fontSize = parseInt( size/35 )
+          fontSize = 8 if(fontSize < 8)
+          fontSize + 'px'
+        )
+        .each( (d) ->
+          
+          # beautify label
+          n = beautifyLabel( data.entities[d.index].label )
+          
+          # get the current element
+          text = d3.select(this)
+            .attr("dy", n.length / 3 - (n.length-1) * 0.9 + 'em')
+            .html(n[0])
+    
+          # now loop
+          for i in [1..n.length]
+            text.append("tspan")
+            .attr('x', 0)
+            .attr('dy', '1em')
+            .html(n[i])
+    
+          text.attr('transform', (d) ->
+            alpha = d.startAngle - Math.PI/2 + Math.abs((d.endAngle - d.startAngle)/2)
+            labelWidth = 3
+            labelAngle = undefined
+            if(alpha > Math.PI/2)
+              labelAngle = alpha - Math.PI
+              labelWidth += d3.select(this)[0][0].clientWidth
+            else
+              labelAngle = alpha
+    
+            labelAngle = rad2deg( labelAngle )
+    
+            r = (outerRadius + labelWidth)/size
+            x = 0.5 + ( r * Math.cos(alpha) )
+            y = 0.5 + ( r * Math.sin(alpha) )
+    
+            translate(x, y, size) + rotate( labelAngle )
+          )
+    
       )
-
-  )
-
-  # Creating an hidden tooltip.
-  tooltip = d3.select('body').append('div')
-    .attr('class', 'tooltip')
-    .style('background-color', 'white')
-    .style('opacity', 0.0)
-    .style('position', 'absolute')
-    .style('z-index', 100)
-
-  #  Dynamic behavior for entities.
-  viz.selectAll('.entity, .label')
-    .on('mouseover', (c) ->
-      d3.select(this).attr('cursor','pointer')
-      viz.selectAll('.relation')
-        .filter( (d, i) -> d.source.index is c.index or d.target.index is c.index )
-        .style( 'opacity', 0.8)
-
-      # Show tooltip.
-      tooltip.text( data.entities[c.index].label ).style('opacity', 1.0 )
-    )
-    .on('mouseout', (c) ->
-      viz.selectAll('.relation')
-        .filter( (d, i) -> d.source.index is c.index or d.target.index is c.index )
-        .style( 'opacity', 0.2 )
-
-        # Hide tooltip.
-        tooltip.style('opacity', 0.0)
-    )
-    .on('mousemove', ->
-      # Change tooltip position.
-      tooltip.style("left", (d3.event.pageX) + "px")
-        .style("top", (d3.event.pageY - 30) + "px")
-    )
-    .on('click', (d) ->
-      url = data.entities[d.index].url
-      window.location = url
-    )
+    
+      # Creating an hidden tooltip.
+      tooltip = d3.select('body').append('div')
+        .attr('class', 'tooltip')
+        .style('background-color', 'white')
+        .style('opacity', 0.0)
+        .style('position', 'absolute')
+        .style('z-index', 100)
+    
+      #  Dynamic behavior for entities.
+      viz.selectAll('.entity, .label')
+        .on('mouseover', (c) ->
+          d3.select(this).attr('cursor','pointer')
+          viz.selectAll('.relation')
+            .filter( (d, i) -> d.source.index is c.index or d.target.index is c.index )
+            .style( 'opacity', 0.8)
+    
+          # Show tooltip.
+          tooltip.text( data.entities[c.index].label ).style('opacity', 1.0 )
+        )
+        .on('mouseout', (c) ->
+          viz.selectAll('.relation')
+            .filter( (d, i) -> d.source.index is c.index or d.target.index is c.index )
+            .style( 'opacity', 0.2 )
+    
+            # Hide tooltip.
+            tooltip.style('opacity', 0.0)
+        )
+        .on('mousemove', ->
+          # Change tooltip position.
+          tooltip.style("left", (d3.event.pageX) + "px")
+            .style("top", (d3.event.pageY - 30) + "px")
+        )
+        .on('click', (d) ->
+          url = data.entities[d.index].url
+          window.location = url
+        )
+     
+     # start chord operations   
+     init()
 
 jQuery ($) ->
   $('.wl-chord').each ->
@@ -206,7 +229,7 @@ jQuery ($) ->
     $.extend params, wl_chord_params
     
     # Launch chord.
-    getChordData params
+    $(this).chord params
 
 jQuery ($) ->
   $('.wl-timeline').each ->
--- dist/3.0.0-beta/js/wordlift.ui.js
+++ dist/3.0.0-beta/js/wordlift.ui.js
@@ -1,174 +1,196 @@
 (function() {
-  var $, buildChord, getChordData;
+  var $;
 
   $ = jQuery;
 
-  getChordData = function(params) {
-    return $.post(params.ajax_url, {
-      action: params.action,
-      post_id: params.postId,
-      depth: params.depth
-    }, function(data) {
-      return buildChord(data, params);
-    });
-  };
-
-  buildChord = function(data, params) {
-    var arc, beautifyLabel, chord, colorLuminance, e, entity, getEntityIndex, height, innerRadius, matrix, outerRadius, rad2deg, relation, rotate, sign, size, tooltip, translate, viz, width, x, y, _i, _j, _len, _len1, _ref, _ref1;
-    if (data.entities.length < 2) {
-      return;
-    }
-    translate = function(x, y, size) {
-      return 'translate(' + x * size + ',' + y * size + ')';
-    };
-    rotate = function(x) {
-      return "rotate(" + x + ")";
-    };
-    rad2deg = function(a) {
-      return (a / (2 * Math.PI)) * 360;
-    };
-    sign = function(n) {
-      if (n >= 0.0) {
-        return 1;
-      } else {
-        return -1;
-      }
-    };
-    beautifyLabel = function(txt) {
-      if (txt.length > 12) {
-        return txt.substring(0, 12) + '...';
-      }
-      return txt;
-    };
-    colorLuminance = function(hex, lum) {
-      var c, i, rgb, _i;
-      hex = String(hex).replace(/[^0-9a-f]/gi, '');
-      if (hex.length < 6) {
-        hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
-      }
-      lum = lum || 0;
-      rgb = "#";
-      c = void 0;
-      i = void 0;
-      for (i = _i = 0; _i <= 3; i = ++_i) {
-        c = parseInt(hex.substr(i * 2, 2), 16);
-        c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
-        rgb += ("00" + c).substr(c.length);
-      }
-      return rgb;
-    };
-    getEntityIndex = function(uri) {
-      var i, _i, _ref;
-      for (i = _i = 0, _ref = data.entities.length; 0 <= _ref ? _i <= _ref : _i >= _ref; i = 0 <= _ref ? ++_i : --_i) {
-        if (data.entities[i].uri === uri) {
-          return i;
+  $.fn.extend({
+    chord: function(options) {
+      var buildChord, getChordData, init, settings;
+      settings = {};
+      settings = $.extend(settings, options);
+      init = function() {
+        return getChordData();
+      };
+      getChordData = function() {
+        return $.post(settings.ajax_url, {
+          action: settings.action,
+          post_id: settings.postId,
+          depth: settings.depth
+        }, function(data) {
+          return buildChord(data);
+        });
+      };
+      buildChord = function(data) {
+        var arc, beautifyLabel, chord, colorLuminance, e, entity, getEntityIndex, height, innerRadius, matrix, outerRadius, rad2deg, relation, rotate, sign, size, tooltip, translate, viz, width, x, y, _i, _j, _len, _len1, _ref, _ref1;
+        if (data.entities.length < 2) {
+          return;
         }
-      }
-      return -1;
-    };
-    matrix = [];
-    _ref = data.entities;
-    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-      entity = _ref[_i];
-      matrix.push((function() {
-        var _j, _len1, _ref1, _results;
-        _ref1 = data.entities;
-        _results = [];
+        translate = function(x, y, size) {
+          return 'translate(' + x * size + ',' + y * size + ')';
+        };
+        rotate = function(x) {
+          return "rotate(" + x + ")";
+        };
+        rad2deg = function(a) {
+          return (a / (2 * Math.PI)) * 360;
+        };
+        sign = function(n) {
+          if (n >= 0.0) {
+            return 1;
+          } else {
+            return -1;
+          }
+        };
+        beautifyLabel = function(words) {
+          var maxLabelLenght, maxWordLength, n, w, _i, _ref;
+          maxLabelLenght = 30;
+          if (words.length > maxLabelLenght) {
+            words = words.substring(0, maxLabelLenght) + '...';
+          }
+          words = words.split(/\s/);
+          n = [];
+          maxWordLength = 5;
+          for (w = _i = 0, _ref = words.length - 1; 0 <= _ref ? _i <= _ref : _i >= _ref; w = 0 <= _ref ? ++_i : --_i) {
+            if (words[w].length > maxWordLength || w === words.length - 1) {
+              n.push(words[w]);
+            } else {
+              words[w + 1] = words[w] + ' ' + words[w + 1];
+            }
+          }
+          return n;
+        };
+        colorLuminance = function(hex, lum) {
+          var c, i, rgb, _i;
+          hex = String(hex).replace(/[^0-9a-f]/gi, '');
+          if (hex.length < 6) {
+            hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+          }
+          lum = lum || 0;
+          rgb = "#";
+          c = void 0;
+          i = void 0;
+          for (i = _i = 0; _i <= 3; i = ++_i) {
+            c = parseInt(hex.substr(i * 2, 2), 16);
+            c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
+            rgb += ("00" + c).substr(c.length);
+          }
+          return rgb;
+        };
+        getEntityIndex = function(uri) {
+          var i, _i, _ref;
+          for (i = _i = 0, _ref = data.entities.length; 0 <= _ref ? _i <= _ref : _i >= _ref; i = 0 <= _ref ? ++_i : --_i) {
+            if (data.entities[i].uri === uri) {
+              return i;
+            }
+          }
+          return -1;
+        };
+        matrix = [];
+        _ref = data.entities;
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          entity = _ref[_i];
+          matrix.push((function() {
+            var _j, _len1, _ref1, _results;
+            _ref1 = data.entities;
+            _results = [];
+            for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
+              e = _ref1[_j];
+              _results.push(0);
+            }
+            return _results;
+          })());
+        }
+        _ref1 = data.relations;
         for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
-          e = _ref1[_j];
-          _results.push(0);
+          relation = _ref1[_j];
+          x = getEntityIndex(relation.s);
+          y = getEntityIndex(relation.o);
+          matrix[x][y] = 1;
+          matrix[y][x] = 1;
         }
-        return _results;
-      })());
+        viz = d3.select('#' + settings.widget_id).append('svg');
+        viz.attr('width', '100%').attr('height', '100%');
+        width = parseInt(viz.style('width'));
+        height = parseInt(viz.style('height'));
+        size = height < width ? height : width;
+        innerRadius = size * 0.2;
+        outerRadius = size * 0.25;
+        arc = d3.svg.arc().innerRadius(innerRadius).outerRadius(outerRadius);
+        chord = d3.layout.chord().padding(0.3).matrix(matrix);
+        viz.selectAll('chords').data(chord.chords).enter().append('path').attr('class', 'relation').attr('d', d3.svg.chord().radius(innerRadius)).attr('transform', translate(0.5, 0.5, size)).style('opacity', 0.2).on('mouseover', function() {
+          return d3.select(this).style('opacity', 0.8);
+        }).on('mouseout', function() {
+          return d3.select(this).style('opacity', 0.2);
+        });
+        viz.selectAll('arcs').data(chord.groups).enter().append('path').attr('class', function(d) {
+          return "entity " + data.entities[d.index].css_class;
+        }).attr('d', arc).attr('transform', translate(0.5, 0.5, size)).style('fill', function(d) {
+          var baseColor, type;
+          baseColor = settings.mainColor;
+          type = data.entities[d.index].type;
+          if (type === 'post') {
+            return baseColor;
+          }
+          if (type === 'entity') {
+            return colorLuminance(baseColor, -0.5);
+          }
+          return colorLuminance(baseColor, 0.5);
+        });
+        viz.selectAll('arcs_labels').data(chord.groups).enter().append('text').attr('class', 'label').attr('font-size', function() {
+          var fontSize;
+          fontSize = parseInt(size / 35);
+          if (fontSize < 8) {
+            fontSize = 8;
+          }
+          return fontSize + 'px';
+        }).each(function(d) {
+          var i, n, text, _k, _ref2;
+          n = beautifyLabel(data.entities[d.index].label);
+          text = d3.select(this).attr("dy", n.length / 3 - (n.length - 1) * 0.9 + 'em').html(n[0]);
+          for (i = _k = 1, _ref2 = n.length; 1 <= _ref2 ? _k <= _ref2 : _k >= _ref2; i = 1 <= _ref2 ? ++_k : --_k) {
+            text.append("tspan").attr('x', 0).attr('dy', '1em').html(n[i]);
+          }
+          return text.attr('transform', function(d) {
+            var alpha, labelAngle, labelWidth, r;
+            alpha = d.startAngle - Math.PI / 2 + Math.abs((d.endAngle - d.startAngle) / 2);
+            labelWidth = 3;
+            labelAngle = void 0;
+            if (alpha > Math.PI / 2) {
+              labelAngle = alpha - Math.PI;
+              labelWidth += d3.select(this)[0][0].clientWidth;
+            } else {
+              labelAngle = alpha;
+            }
+            labelAngle = rad2deg(labelAngle);
+            r = (outerRadius + labelWidth) / size;
+            x = 0.5 + (r * Math.cos(alpha));
+            y = 0.5 + (r * Math.sin(alpha));
+            return translate(x, y, size) + rotate(labelAngle);
+          });
+        });
+        tooltip = d3.select('body').append('div').attr('class', 'tooltip').style('background-color', 'white').style('opacity', 0.0).style('position', 'absolute').style('z-index', 100);
+        return viz.selectAll('.entity, .label').on('mouseover', function(c) {
+          d3.select(this).attr('cursor', 'pointer');
+          viz.selectAll('.relation').filter(function(d, i) {
+            return d.source.index === c.index || d.target.index === c.index;
+          }).style('opacity', 0.8);
+          return tooltip.text(data.entities[c.index].label).style('opacity', 1.0);
+        }).on('mouseout', function(c) {
+          viz.selectAll('.relation').filter(function(d, i) {
+            return d.source.index === c.index || d.target.index === c.index;
+          }).style('opacity', 0.2);
+          return tooltip.style('opacity', 0.0);
+        }).on('mousemove', function() {
+          return tooltip.style("left", d3.event.pageX + "px").style("top", (d3.event.pageY - 30) + "px");
+        }).on('click', function(d) {
+          var url;
+          url = data.entities[d.index].url;
+          return window.location = url;
+        });
+      };
+      return init();
     }
-    _ref1 = data.relations;
-    for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
-      relation = _ref1[_j];
-      x = getEntityIndex(relation.s);
-      y = getEntityIndex(relation.o);
-      matrix[x][y] = 1;
-      matrix[y][x] = 1;
-    }
-    viz = d3.select('#' + params.widget_id).append('svg');
-    viz.attr('width', '100%').attr('height', '100%');
-    width = parseInt(viz.style('width'));
-    height = parseInt(viz.style('height'));
-    size = height < width ? height : width;
-    innerRadius = size * 0.2;
-    outerRadius = size * 0.25;
-    arc = d3.svg.arc().innerRadius(innerRadius).outerRadius(outerRadius);
-    chord = d3.layout.chord().padding(0.3).matrix(matrix);
-    viz.selectAll('chords').data(chord.chords).enter().append('path').attr('class', 'relation').attr('d', d3.svg.chord().radius(innerRadius)).attr('transform', translate(0.5, 0.5, size)).style('opacity', 0.2).on('mouseover', function() {
-      return d3.select(this).style('opacity', 0.8);
-    }).on('mouseout', function() {
-      return d3.select(this).style('opacity', 0.2);
-    });
-    viz.selectAll('arcs').data(chord.groups).enter().append('path').attr('class', function(d) {
-      return "entity " + data.entities[d.index].css_class;
-    }).attr('d', arc).attr('transform', translate(0.5, 0.5, size)).style('fill', function(d) {
-      var baseColor, type;
-      baseColor = params.mainColor;
-      type = data.entities[d.index].type;
-      if (type === 'post') {
-        return baseColor;
-      }
-      if (type === 'entity') {
-        return colorLuminance(baseColor, -0.5);
-      }
-      return colorLuminance(baseColor, 0.5);
-    });
-    viz.selectAll('arcs_labels').data(chord.groups).enter().append('text').attr('class', 'label').attr('font-size', function() {
-      var fontSize;
-      fontSize = parseInt(size / 35);
-      if (fontSize < 8) {
-        fontSize = 8;
-      }
-      return fontSize + 'px';
-    }).each(function(d) {
-      var i, n, text, _k, _ref2;
-      n = data.entities[d.index].label.split(/\s/);
-      text = d3.select(this).attr("dy", n.length / 3 - (n.length - 1) * 0.9 + 'em').html(n[0]);
-      for (i = _k = 1, _ref2 = n.length; 1 <= _ref2 ? _k <= _ref2 : _k >= _ref2; i = 1 <= _ref2 ? ++_k : --_k) {
-        text.append("tspan").attr('x', 0).attr('dy', '1em').html(n[i]);
-      }
-      return text.attr('transform', function(d) {
-        var alpha, labelAngle, labelWidth, r;
-        alpha = d.startAngle - Math.PI / 2 + Math.abs((d.endAngle - d.startAngle) / 2);
-        labelWidth = 3;
-        labelAngle = void 0;
-        if (alpha > Math.PI / 2) {
-          labelAngle = alpha - Math.PI;
-          labelWidth += d3.select(this)[0][0].clientWidth;
-        } else {
-          labelAngle = alpha;
-        }
-        labelAngle = rad2deg(labelAngle);
-        r = (outerRadius + labelWidth) / size;
-        x = 0.5 + (r * Math.cos(alpha));
-        y = 0.5 + (r * Math.sin(alpha));
-        return translate(x, y, size) + rotate(labelAngle);
-      });
-    });
-    tooltip = d3.select('body').append('div').attr('class', 'tooltip').style('background-color', 'white').style('opacity', 0.0).style('position', 'absolute').style('z-index', 100);
-    return viz.selectAll('.entity, .label').on('mouseover', function(c) {
-      d3.select(this).attr('cursor', 'pointer');
-      viz.selectAll('.relation').filter(function(d, i) {
-        return d.source.index === c.index || d.target.index === c.index;
-      }).style('opacity', 0.8);
-      return tooltip.text(data.entities[c.index].label).style('opacity', 1.0);
-    }).on('mouseout', function(c) {
-      viz.selectAll('.relation').filter(function(d, i) {
-        return d.source.index === c.index || d.target.index === c.index;
-      }).style('opacity', 0.2);
-      return tooltip.style('opacity', 0.0);
-    }).on('mousemove', function() {
-      return tooltip.style("left", d3.event.pageX + "px").style("top", (d3.event.pageY - 30) + "px");
-    }).on('click', function(d) {
-      var url;
-      url = data.entities[d.index].url;
-      return window.location = url;
-    });
-  };
+  });
 
   jQuery(function($) {
     return $('.wl-chord').each(function() {
@@ -176,7 +198,7 @@
       params = $(this).data();
       params.widget_id = $(this).attr('id');
       $.extend(params, wl_chord_params);
-      return getChordData(params);
+      return $(this).chord(params);
     });
   });
 
