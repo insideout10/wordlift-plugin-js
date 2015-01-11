852f99279d7fafeffb805c79ac5aef4a849860a8
Marcello Colacino <marcello.colacino@gmail.com>
2015-01-11 19:10:34 +0100

    Embed tinymce editor missing files

---
+++ app/js/wordlift-reloaded.js
@@ -0,0 +1,338 @@
+(function() {
+  var $, Traslator, container, injector,
+    __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
+
+  Traslator = (function() {
+    Traslator.prototype._htmlPositions = [];
+
+    Traslator.prototype._textPositions = [];
+
+    Traslator.prototype._html = '';
+
+    Traslator.prototype._text = '';
+
+    Traslator.create = function(html) {
+      var traslator;
+      traslator = new Traslator(html);
+      traslator.parse();
+      return traslator;
+    };
+
+    function Traslator(html) {
+      this._html = html;
+    }
+
+    Traslator.prototype.parse = function() {
+      var htmlElem, htmlLength, htmlPost, htmlPre, match, pattern, textLength, textPost, textPre;
+      this._htmlPositions = [];
+      this._textPositions = [];
+      this._text = '';
+      this._html = this._html.replace(/&nbsp;/gim, ' ');
+      pattern = /([^<]*)(<[^>]*>)([^<]*)/gim;
+      textLength = 0;
+      htmlLength = 0;
+      while (match = pattern.exec(this._html)) {
+        htmlPre = match[1];
+        htmlElem = match[2];
+        htmlPost = match[3];
+        textPre = htmlPre + ('</p>' === htmlElem.toLowerCase() ? '\n\n' : '');
+        textPost = htmlPost;
+        textLength += textPre.length;
+        htmlLength += htmlPre.length + htmlElem.length;
+        this._htmlPositions.push(htmlLength);
+        this._textPositions.push(textLength);
+        textLength += textPost.length;
+        htmlLength += htmlPost.length;
+        this._text += textPre + textPost;
+      }
+      if ('' === this._text && '' !== this._html) {
+        this._text = new String(this._html);
+      }
+      if (0 === this._textPositions.length || 0 !== this._textPositions[0]) {
+        this._htmlPositions.unshift(0);
+        return this._textPositions.unshift(0);
+      }
+    };
+
+    Traslator.prototype.text2html = function(pos) {
+      var htmlPos, i, textPos, _i, _ref;
+      htmlPos = 0;
+      textPos = 0;
+      for (i = _i = 0, _ref = this._textPositions.length; 0 <= _ref ? _i < _ref : _i > _ref; i = 0 <= _ref ? ++_i : --_i) {
+        if (pos < this._textPositions[i]) {
+          break;
+        }
+        htmlPos = this._htmlPositions[i];
+        textPos = this._textPositions[i];
+      }
+      return htmlPos + pos - textPos;
+    };
+
+    Traslator.prototype.html2text = function(pos) {
+      var htmlPos, i, textPos, _i, _ref;
+      if (pos < this._htmlPositions[0]) {
+        return 0;
+      }
+      htmlPos = 0;
+      textPos = 0;
+      for (i = _i = 0, _ref = this._htmlPositions.length; 0 <= _ref ? _i < _ref : _i > _ref; i = 0 <= _ref ? ++_i : --_i) {
+        if (pos < this._htmlPositions[i]) {
+          break;
+        }
+        htmlPos = this._htmlPositions[i];
+        textPos = this._textPositions[i];
+      }
+      return textPos + pos - htmlPos;
+    };
+
+    Traslator.prototype.insertHtml = function(fragment, pos) {
+      var htmlPos;
+      htmlPos = this.text2html(pos.text);
+      this._html = this._html.substring(0, htmlPos) + fragment + this._html.substring(htmlPos);
+      return this.parse();
+    };
+
+    Traslator.prototype.getHtml = function() {
+      return this._html;
+    };
+
+    Traslator.prototype.getText = function() {
+      return this._text;
+    };
+
+    return Traslator;
+
+  })();
+
+  window.Traslator = Traslator;
+
+  $ = jQuery;
+
+  angular.module('wordlift.core', []).service('AnalysisService', [
+    '$log', '$http', '$rootScope', function($log, $http, $rootScope) {
+      var service, _currentAnalysis;
+      service = _currentAnalysis = {};
+      service.parse = function(data) {
+        var annotation, ea, entity, id, _i, _len, _ref, _ref1, _ref2;
+        _ref = data.entities;
+        for (id in _ref) {
+          entity = _ref[id];
+          entity.occurrences = 0;
+          entity.id = id;
+          entity.annotations = {};
+          entity.isRelatedToAnnotation = function(annotationId) {
+            if (this.annotations[annotationId] != null) {
+              return true;
+            } else {
+              return false;
+            }
+          };
+        }
+        _ref1 = data.annotations;
+        for (id in _ref1) {
+          annotation = _ref1[id];
+          _ref2 = annotation.entityMatches;
+          for (_i = 0, _len = _ref2.length; _i < _len; _i++) {
+            ea = _ref2[_i];
+            data.entities[ea.entityId].annotations[id] = annotation;
+          }
+        }
+        return data;
+      };
+      service.perform = function() {
+        return $http({
+          method: 'get',
+          url: 'assets/sample-response.json'
+        }).success(function(data) {
+          return $rootScope.$broadcast("analysisPerformed", service.parse(data));
+        });
+      };
+      return service;
+    }
+  ]).service('ConfigurationService', [
+    '$log', '$http', '$rootScope', function($log, $http, $rootScope) {
+      var service, _configuration;
+      service = _configuration = {};
+      service.loadConfiguration = function() {
+        return $http({
+          method: 'get',
+          url: 'assets/sample-widget-configuration.json'
+        }).success(function(data) {
+          return $rootScope.$broadcast("configurationLoaded", data);
+        });
+      };
+      return service;
+    }
+  ]).controller('EditPostWidgetController', [
+    '$log', '$scope', '$rootScope', function($log, $scope, $rootScope) {
+      $scope.configuration = [];
+      $scope.analysis = {};
+      $scope.selectedEntities = {};
+      $scope.annotation = void 0;
+      $scope.boxes = [];
+      $scope.addBox = function(scope, id) {
+        return $scope.boxes[id] = scope;
+      };
+      $scope.$on("configurationLoaded", function(event, configuration) {
+        var box, _i, _len, _ref;
+        _ref = configuration.classificationBoxes;
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          box = _ref[_i];
+          $scope.selectedEntities[box.id] = {};
+        }
+        return $scope.configuration = configuration;
+      });
+      $scope.$on("analysisPerformed", function(event, analysis) {
+        $log.debug(analysis);
+        return $scope.analysis = analysis;
+      });
+      return $scope.onSelectedEntityTile = function(entity, scope) {
+        $log.debug("Entity tile selected for entity " + entity.id + " within '" + scope + "' scope");
+        if ($scope.selectedEntities[scope][entity.id] == null) {
+          $scope.selectedEntities[scope][entity.id] = entity;
+          return $log.debug($scope.selectedEntities);
+        } else {
+          delete $scope.selectedEntities[scope][entity.id];
+          return $scope.boxes[scope].deselect(entity);
+        }
+      };
+    }
+  ]).directive('wlClassificationBox', [
+    '$log', function($log) {
+      return {
+        restrict: 'E',
+        scope: true,
+        template: "    	<div class=\"classification-box\">\n    		<div class=\"box-header\">\n          <h5 class=\"label\">{{box.label}}</h5>\n          <span ng-class=\"'wl-' + entity.mainType\" ng-repeat=\"(id, entity) in selectedEntities[box.id]\" class=\"wl-selected-item\">\n            {{ entity.label}}\n            <i class=\"wl-deselect-item\" ng-click=\"onSelectedEntityTile(entity, box.id)\"></i>\n          </span>\n        </div>\n	<wl-entity-tile notify=\"onSelectedEntityTile(entity.id, box.id)\" entity=\"entity\" ng-repeat=\"entity in entities\"></wl-entity>\n</div>	",
+        link: function($scope, $element, $attrs, $ctrl) {
+          var entity, id, _ref, _ref1, _results;
+          $scope.entities = {};
+          _ref = $scope.analysis.entities;
+          _results = [];
+          for (id in _ref) {
+            entity = _ref[id];
+            if (_ref1 = entity.mainType, __indexOf.call($scope.box.registeredTypes, _ref1) >= 0) {
+              _results.push($scope.entities[id] = entity);
+            } else {
+              _results.push(void 0);
+            }
+          }
+          return _results;
+        },
+        controller: function($scope, $element, $attrs) {
+          var ctrl;
+          $scope.tiles = [];
+          $scope.addBox($scope, $scope.box.id);
+          $scope.deselect = function(entity) {
+            var tile, _i, _len, _ref, _results;
+            _ref = $scope.tiles;
+            _results = [];
+            for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+              tile = _ref[_i];
+              if (tile.entity.id === entity.id) {
+                _results.push(tile.isSelected = false);
+              } else {
+                _results.push(void 0);
+              }
+            }
+            return _results;
+          };
+          $scope.$watch("annotation", function(annotationId) {
+            var tile, _i, _len, _ref, _results;
+            $log.debug("annotation " + annotationId);
+            if (annotationId == null) {
+              return;
+            }
+            _ref = $scope.tiles;
+            _results = [];
+            for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+              tile = _ref[_i];
+              _results.push(tile.isVisible = tile.entity.isRelatedToAnnotation(annotationId));
+            }
+            return _results;
+          });
+          ctrl = {
+            onSelectedTile: function(tile) {
+              tile.isSelected = !tile.isSelected;
+              return $scope.onSelectedEntityTile(tile.entity, $scope.box.id);
+            },
+            addTile: function(tile) {
+              $log.debug("Adding tile with id " + tile.$id);
+              return $scope.tiles.push(tile);
+            },
+            closeTiles: function() {
+              var tile, _i, _len, _ref, _results;
+              _ref = $scope.tiles;
+              _results = [];
+              for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+                tile = _ref[_i];
+                _results.push(tile.close());
+              }
+              return _results;
+            }
+          };
+          return ctrl;
+        }
+      };
+    }
+  ]).directive('wlEntityTile', [
+    '$log', function($log) {
+      return {
+        require: '^wlClassificationBox',
+        restrict: 'E',
+        scope: {
+          entity: '='
+        },
+        template: "<div ng-class=\"wrapperCssClasses\" ng-show=\"isVisible\">\n  <i ng-class=\"{ 'wl-selected' : isSelected, 'wl-unselected' : !isSelected }\"></i>\n        <i class=\"type\"></i>\n        <span class=\"label\" ng-click=\"select()\">{{entity.label}}</span>\n        <small ng-show=\"entity.occurrences > 0\">({{entity.occurrences}})</small>\n        <i ng-class=\"{ 'wl-more': isOpened == false, 'wl-less': isOpened == true }\" ng-click=\"toggle()\"></i>\n  <div class=\"details\" ng-show=\"isOpened\">\n          <p><img class=\"thumbnail\" ng-src=\"{{ entity.images[0] }}\" />{{entity.description}}</p>\n        </div>\n</div>\n",
+        link: function($scope, $element, $attrs, $ctrl) {
+          $ctrl.addTile($scope);
+          $scope.isOpened = false;
+          $scope.isVisible = true;
+          $scope.isSelected = false;
+          $scope.wrapperCssClasses = ["entity", "wl-" + $scope.entity.mainType];
+          $scope.open = function() {
+            return $scope.isOpened = true;
+          };
+          $scope.close = function() {
+            return $scope.isOpened = false;
+          };
+          $scope.toggle = function() {
+            if (!$scope.isOpened) {
+              $ctrl.closeTiles();
+            }
+            return $scope.isOpened = !$scope.isOpened;
+          };
+          return $scope.select = function() {
+            return $ctrl.onSelectedTile($scope);
+          };
+        }
+      };
+    }
+  ]);
+
+  $(container = $("<div id=\"wordlift-edit-post-wrapper\" ng-controller=\"EditPostWidgetController\">\n	<wl-classification-box ng-repeat=\"box in configuration.classificationBoxes\"></wl-classification-box>\n    </div>\n").appendTo('#dx'), injector = angular.bootstrap($('body'), ['wordlift.core']), injector.invoke([
+    'ConfigurationService', 'AnalysisService', '$rootScope', function(ConfigurationService, AnalysisService, $rootScope) {
+      return $rootScope.$apply(function() {
+        AnalysisService.perform();
+        return ConfigurationService.loadConfiguration();
+      });
+    }
+  ]), tinymce.PluginManager.add('wordlift', function(editor, url) {
+    editor.onLoadContent.add(function(ed, o) {});
+    editor.addButton('wordlift_add_entity', {
+      classes: 'widget btn wordlift_add_entity',
+      text: ' ',
+      tooltip: 'Insert entity',
+      onclick: function() {}
+    });
+    editor.addButton('wordlift', {
+      classes: 'widget btn wordlift',
+      text: ' ',
+      tooltip: 'Analyse',
+      onclick: function() {}
+    });
+    return editor.onClick.add(function(editor, e) {});
+  }));
+
+}).call(this);
+
+//# sourceMappingURL=wordlift-reloaded.js.map
---
+++ app/js/wordlift-reloaded.src.coffee
@@ -0,0 +1,389 @@
+class Traslator
+
+  # Hold the html and textual positions.
+  _htmlPositions: []
+  _textPositions: []
+
+  # Hold the html and text contents.
+  _html: ''
+  _text: ''
+
+  # Create an instance of the traslator.
+  @create: (html) ->
+    traslator = new Traslator(html)
+    traslator.parse()
+    traslator
+
+  constructor: (html) ->
+    @_html = html
+
+  parse: ->
+    @_htmlPositions = []
+    @_textPositions = []
+    @_text = ''
+
+    # TODO: the pattern should consider that HTML has also HTML entities.
+    # Remove non-breaking spaces.
+    @_html = @_html.replace /&nbsp;/gim, ' '
+
+    pattern = /([^<]*)(<[^>]*>)([^<]*)/gim
+
+    textLength = 0
+    htmlLength = 0
+
+    while match = pattern.exec @_html
+
+      # Get the text pre/post and the html element
+      htmlPre = match[1]
+      htmlElem = match[2]
+      htmlPost = match[3]
+
+      # Get the text pre/post w/o new lines.
+      textPre = htmlPre + (if '</p>' is htmlElem.toLowerCase() then '\n\n' else '')
+#      dump "[ htmlPre length :: #{htmlPre.length} ][ textPre length :: #{textPre.length} ]"
+      textPost = htmlPost
+
+      # Sum the lengths to the existing lengths.
+      textLength += textPre.length
+      # For html add the length of the html element.
+      htmlLength += htmlPre.length + htmlElem.length
+
+      # Add the position.
+      @_htmlPositions.push htmlLength
+      @_textPositions.push textLength
+
+      textLength += textPost.length
+      htmlLength += htmlPost.length
+
+      # Add the textual parts to the text.
+      @_text += textPre + textPost
+
+
+    # In case the regex didn't find any tag, copy the html over the text.
+    @_text = new String(@_html) if '' is @_text and '' isnt @_html
+
+    # Add text position 0 if it's not already set.
+    if 0 is @_textPositions.length or 0 isnt @_textPositions[0]
+      @_htmlPositions.unshift 0
+      @_textPositions.unshift 0
+
+#    console.log '=============================='
+#    console.log @_html
+#    console.log @_text
+#    console.log @_htmlPositions
+#    console.log @_textPositions
+#    console.log '=============================='
+
+  # Get the html position, given a text position.
+  text2html: (pos) ->
+    htmlPos = 0
+    textPos = 0
+
+    for i in [0...@_textPositions.length]
+      break if pos < @_textPositions[i]
+      htmlPos = @_htmlPositions[i]
+      textPos = @_textPositions[i]
+
+    #    dump "#{htmlPos} + #{pos} - #{textPos}"
+    htmlPos + pos - textPos
+
+  # Get the text position, given an html position.
+  html2text: (pos) ->
+#    dump @_htmlPositions
+#    dump @_textPositions
+
+    # Return 0 if the specified html position is less than the first HTML position.
+    return 0 if pos < @_htmlPositions[0]
+
+    htmlPos = 0
+    textPos = 0
+
+    for i in [0...@_htmlPositions.length]
+      break if pos < @_htmlPositions[i]
+      htmlPos = @_htmlPositions[i]
+      textPos = @_textPositions[i]
+
+#    console.log "#{textPos} + #{pos} - #{htmlPos}"
+    textPos + pos - htmlPos
+
+  # Insert an Html fragment at the specified location.
+  insertHtml: (fragment, pos) ->
+
+#    dump @_htmlPositions
+#    dump @_textPositions
+#    console.log "[ fragment :: #{fragment} ][ pos text :: #{pos.text} ]"
+
+    htmlPos = @text2html pos.text
+
+    @_html = @_html.substring(0, htmlPos) + fragment + @_html.substring(htmlPos)
+
+    # Reparse
+    @parse()
+
+  # Return the html.
+  getHtml: ->
+    @_html
+
+  # Return the text.
+  getText: ->
+    @_text
+
+window.Traslator = Traslator
+# Set the well-known $ reference to jQuery.
+$ = jQuery
+
+# Create the main AngularJS module, and set it dependent on controllers and directives.
+angular.module('wordlift.core', [])
+
+# Manage redlink analysis responses
+.service('AnalysisService', [ '$log', '$http', '$rootScope', ($log, $http, $rootScope)-> 
+	
+  service = 
+    _currentAnalysis = {}
+
+  service.parse = (data) ->
+    
+    for id, entity of data.entities
+      entity.occurrences = 0
+      entity.id = id
+      entity.annotations = {}
+      entity.isRelatedToAnnotation = (annotationId)->
+        if @.annotations[ annotationId ]? then true else false
+
+    for id, annotation of data.annotations
+      for ea in annotation.entityMatches
+      	data.entities[ ea.entityId ].annotations[ id ] = annotation
+
+    data
+
+  service.perform = ()->
+  	$http(
+      method: 'get'
+      url: 'assets/sample-response.json'
+    )
+    # If successful, broadcast an *analysisReceived* event.
+    .success (data) ->
+       $rootScope.$broadcast "analysisPerformed", service.parse( data )
+
+  service
+
+])
+
+# Manage redlink analysis responses
+.service('ConfigurationService', [ '$log', '$http', '$rootScope', ($log, $http, $rootScope)-> 
+	
+  service = 
+    _configuration = {}
+
+  service.loadConfiguration = ()->
+  	$http(
+      method: 'get'
+      url: 'assets/sample-widget-configuration.json'
+    )
+    .success (data) ->
+      $rootScope.$broadcast "configurationLoaded", data
+  service
+
+])
+
+# Manage redlink analysis responses
+.controller('EditPostWidgetController', [ '$log', '$scope', '$rootScope', ($log, $scope, $rootScope)-> 
+
+  $scope.configuration = []
+  $scope.analysis = {}
+  $scope.selectedEntities = {}
+  $scope.annotation = undefined
+  $scope.boxes = []
+
+  $scope.addBox = (scope, id)->
+    $scope.boxes[id] = scope
+  
+  $scope.$on "configurationLoaded", (event, configuration) ->
+    for box in configuration.classificationBoxes
+      $scope.selectedEntities[ box.id ] = {}
+    $scope.configuration = configuration
+
+  $scope.$on "analysisPerformed", (event, analysis) ->
+    $log.debug analysis
+    $scope.analysis = analysis
+
+  $scope.onSelectedEntityTile = (entity, scope)->
+  	$log.debug "Entity tile selected for entity #{entity.id} within '#{scope}' scope"
+  	if not $scope.selectedEntities[ scope ][ entity.id ]?
+
+  	  $scope.selectedEntities[ scope ][ entity.id ] = entity
+  	  $log.debug $scope.selectedEntities
+  	  # TODO All related annotations has to be disambiguated accordingly
+  	else
+      # TODO Any related annotation has to be reset just if this is the last related instance 
+      delete $scope.selectedEntities[ scope ][ entity.id ]
+      $scope.boxes[ scope ].deselect entity
+      
+])
+.directive('wlClassificationBox', ['$log', ($log)->
+    restrict: 'E'
+    scope: true
+    template: """
+    	<div class="classification-box">
+    		<div class="box-header">
+          <h5 class="label">{{box.label}}</h5>
+          <span ng-class="'wl-' + entity.mainType" ng-repeat="(id, entity) in selectedEntities[box.id]" class="wl-selected-item">
+            {{ entity.label}}
+            <i class="wl-deselect-item" ng-click="onSelectedEntityTile(entity, box.id)"></i>
+          </span>
+        </div>
+  			<wl-entity-tile notify="onSelectedEntityTile(entity.id, box.id)" entity="entity" ng-repeat="entity in entities"></wl-entity>
+  		</div>	
+    """
+    link: ($scope, $element, $attrs, $ctrl) ->  	  
+  	  
+      $scope.entities = {}
+      
+      for id, entity of $scope.analysis.entities
+        if entity.mainType in $scope.box.registeredTypes
+          $scope.entities[ id ] = entity
+
+    controller: ($scope, $element, $attrs) ->
+      
+      # Mantain a reference to nested entity tiles $scope
+      # TODO manage on scope distruction event
+      $scope.tiles = []
+
+      $scope.addBox $scope, $scope.box.id
+
+      $scope.deselect = (entity)->
+        for tile in $scope.tiles
+          tile.isSelected = false if tile.entity.id is entity.id
+
+      $scope.$watch "annotation", (annotationId) ->
+        $log.debug "annotation #{annotationId}"
+        return if not annotationId?
+        for tile in $scope.tiles
+          tile.isVisible = tile.entity.isRelatedToAnnotation( annotationId )
+
+      ctrl =
+        onSelectedTile: (tile)->
+          tile.isSelected = !tile.isSelected
+          $scope.onSelectedEntityTile tile.entity, $scope.box.id
+        addTile: (tile)->
+          $log.debug "Adding tile with id #{tile.$id}"
+          $scope.tiles.push tile
+        closeTiles: ()->
+          for tile in $scope.tiles
+          	tile.close()
+      ctrl
+  ])
+.directive('wlEntityTile', ['$log', ($log)->
+    require: '^wlClassificationBox'
+    restrict: 'E'
+    scope:
+      entity: '='
+    template: """
+  	  <div ng-class="wrapperCssClasses" ng-show="isVisible">
+  	    <i ng-class="{ 'wl-selected' : isSelected, 'wl-unselected' : !isSelected }"></i>
+        <i class="type"></i>
+        <span class="label" ng-click="select()">{{entity.label}}</span>
+        <small ng-show="entity.occurrences > 0">({{entity.occurrences}})</small>
+        <i ng-class="{ 'wl-more': isOpened == false, 'wl-less': isOpened == true }" ng-click="toggle()"></i>
+  	    <div class="details" ng-show="isOpened">
+          <p><img class="thumbnail" ng-src="{{ entity.images[0] }}" />{{entity.description}}</p>
+        </div>
+  	  </div>
+
+  	"""
+    link: ($scope, $element, $attrs, $ctrl) ->				      
+      
+      # Add tile to related container scope
+      $ctrl.addTile $scope
+
+      $scope.isOpened = false
+      $scope.isVisible = true
+      $scope.isSelected = false
+
+      $scope.wrapperCssClasses = [ "entity", "wl-#{$scope.entity.mainType}" ]
+
+      $scope.open = ()->
+      	$scope.isOpened = true
+      $scope.close = ()->
+      	$scope.isOpened = false  	
+      $scope.toggle = ()->
+        if !$scope.isOpened 
+          $ctrl.closeTiles()    
+        $scope.isOpened = !$scope.isOpened
+        
+      $scope.select = ()-> 
+        $ctrl.onSelectedTile $scope
+  ])
+$(
+  container = $("""
+  	<div id="wordlift-edit-post-wrapper" ng-controller="EditPostWidgetController">
+  		<wl-classification-box ng-repeat="box in configuration.classificationBoxes"></wl-classification-box>
+    </div>
+
+  """)
+  .appendTo('#dx')
+
+injector = angular.bootstrap $('body'), ['wordlift.core']
+injector.invoke(['ConfigurationService', 'AnalysisService','$rootScope', (ConfigurationService, AnalysisService, $rootScope) ->
+	# execute the following commands in the angular js context.
+    $rootScope.$apply(->
+    	AnalysisService.perform()
+    	ConfigurationService.loadConfiguration()
+    )
+])
+
+# Add WordLift as a plugin of the TinyMCE editor.
+  tinymce.PluginManager.add 'wordlift', (editor, url) ->
+    editor.onLoadContent.add((ed, o) ->
+      #injector.invoke(['EditorService', (EditorService) ->
+        #EditorService.createDefaultAnalysis()
+      #])
+    )
+    # Add a WordLift button the TinyMCE editor.
+    # TODO Disable the new button as default
+    editor.addButton 'wordlift_add_entity',
+      classes: 'widget btn wordlift_add_entity'
+      text: ' ' # the space is necessary to avoid right spacing on TinyMCE 4
+      tooltip: 'Insert entity'
+      onclick: ->
+
+        #injector.invoke(['EditorService','$rootScope', (EditorService, $rootScope) ->
+          # execute the following commands in the angular js context.
+        #  $rootScope.$apply(->
+            #EditorService.createTextAnnotationFromCurrentSelection()
+        #  )
+        #])
+
+    # Add a WordLift button the TinyMCE editor.
+    editor.addButton 'wordlift',
+      classes: 'widget btn wordlift'
+      text: ' ' # the space is necessary to avoid right spacing on TinyMCE 4
+      tooltip: 'Analyse'
+
+    # When the editor is clicked, the [EditorService.analyze](app.services.EditorService.html#analyze) method is invoked.
+      onclick: ->
+        #injector.invoke(['EditorService', '$rootScope', '$log', (EditorService, $rootScope, $log) ->
+        #  $rootScope.$apply(->
+            # Get the html content of the editor.
+            #html = editor.getContent format: 'raw'
+
+            # Get the text content from the Html.
+            #text = Traslator.create(html).getText()
+
+            # $log.info "onclick [ html :: #{html} ][ text :: #{text} ]"
+            # Send the text content for analysis.
+            #EditorService.analyze text
+        #  )
+        #])
+
+    # TODO: move this outside of this method.
+    # this event is raised when a textannotation is selected in the TinyMCE editor.
+    editor.onClick.add (editor, e) ->
+      #injector.invoke(['$rootScope', ($rootScope) ->
+        # execute the following commands in the angular js context.
+      #  $rootScope.$apply(->
+          # send a message about the currently clicked annotation.
+          #$rootScope.$broadcast 'textAnnotationClicked', e.target.id
+      #  )
+      #])
+)
